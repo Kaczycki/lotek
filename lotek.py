7052d2683b38f69790d40f586e2eda1f6f82a602
Kaczycki <kaczycki@wp.pl>
2022-03-05 05:40:20 +0100

    pyta, losuje, wyświetla liczbę losowań

--- lotek.py
+++ lotek.py
@@ -4,15 +4,18 @@ import random
 wyniki_losowania = set() #wylosowane liczby
 skreslone = set() #skreslone liczby
 trafione = set()
+liczba_losowan = 0
 
-while len(wyniki_losowania) != 6: #losuje 6 niepowtarzalnych liczb od 1 do 49
-    los = random.randrange(1, 50)
-    if los not in wyniki_losowania:
-        wyniki_losowania.add(los)
-    print (wyniki_losowania)
+def wylosuj_6 ():
+    while len(wyniki_losowania) != 6: #losuje 6 niepowtarzalnych liczb od 1 do 49
+        los = random.randrange(1, 10)
+        if los not in wyniki_losowania:
+            wyniki_losowania.add(los)
+    return (wyniki_losowania)
+
 
 while len(skreslone) != 6: #pyta o 6 niepowtarzalnych liczb 1 do 49
-    current_input = input("skreśl swoją liczbę")
+    current_input = int(input('skreśl swoją liczbę '))
     if current_input not in skreslone:
         skreslone.add(current_input)
     print (skreslone)
@@ -21,18 +24,26 @@ trafione = wyniki_losowania.intersection(skreslone)
 
 
 
-while len(trafione) == 0: #szuka wspólnych liczb (skreślonych i wylosowanych)
-  while len(wyniki_losowania) != 6: #losuje 6 niepowtarzalnych liczb od 1 do 49
-    los = random.randrange(1, 50)
-    if los not in wyniki_losowania:
-        wyniki_losowania.add(los)
-    print (wyniki_losowania)
-  trafione = wyniki_losowania.intersection(skreslone)
+while len(trafione) < 6: #szuka wspólnych liczb (skreślonych i wylosowanych)
+    wyniki_losowania=set()
+    while len(wyniki_losowania) != 6: #losuje 6 niepowtarzalnych liczb od 1 do 49
+        los = random.randrange(1, 50)
+        if los not in wyniki_losowania:
+            wyniki_losowania.add(los)
+    trafione = wyniki_losowania.intersection(skreslone)
+    #print(wyniki_losowania)
+    #print ('trafione: ', trafione)
+    liczba_losowan += 1
+    #if liczba_losowan%10000 == 0:
+    #    print (liczba_losowan)
 
 
-print("wygrana!")
+
+print('______')
+print('wygrana!')
 print(wyniki_losowania)
-print(trafione)
+print(trafione, ' za ',liczba_losowan, ' losowaniem')
+
 
 """ input:
     6 liczb
