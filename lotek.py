a2896d2a15c4e9e327eec0128197fbedc36ea811
Kaczycki <kaczycki@wp.pl>
2022-03-05 08:02:34 +0100

    poprawki kosmetyczne

--- lotek.py
+++ lotek.py
@@ -17,7 +17,7 @@ def wylosuj_6 ():
 
 while len(skreslone) != 6: #pyta o 6 niepowtarzalnych liczb 1 do 49
     current_input = int(input('skreśl swoją liczbę '))
-    if current_input not in skreslone and current_input > 0 and current_input < 20:
+    if current_input not in skreslone and current_input > 0 and current_input < 50:
         skreslone.add(current_input)
 print (f'wytypowane liczby, to {skreslone}')
 
@@ -43,7 +43,7 @@ while len(trafione) < 6: #szuka wspólnych liczb (skreślonych i wylosowanych)
 print('______')
 print('wygrana!')
 print(wyniki_losowania)
-print(f'trafione za ,{liczba_losowan}, losowaniem')
+print(f'trafione za {liczba_losowan} losowaniem')
 print(f'wydane {cena*liczba_losowan} złotych, przy cenie {cena} za los. Warto było?')
 print(f'losowania w środy i soboty przyniosły wygraną po {liczba_losowan/2/52} latach')
 
